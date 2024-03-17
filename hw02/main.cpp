78cc07a721094a81862bc5766b7406779bee7052
Nikita Bardatskii <bardanik@fit.cvut.cz>
2024-03-17 22:38:50 +0100

    Implement first version which is able to add positive numbers

--- hw02/main.cpp
+++ hw02/main.cpp
@@ -12,6 +12,7 @@
 #include <iomanip>
 #include <sstream>
 #include <string>
+#include <utility>
 #include <vector>
 #include <algorithm>
 #include <memory>
@@ -25,6 +26,14 @@ using namespace std;
 
 class CBigInt {
 public:
+    bool positive() const {
+        return !m_is_negative;
+    }
+
+    bool negative() const {
+        return m_is_negative;
+    }
+
     // default constructor
     // copying/assignment/destruction
     // int constructor
@@ -36,29 +45,99 @@ public:
             m_is_negative = false;
         }
         m_num = to_string(number);
+        reverse(m_num.begin(), m_num.end());
+
     }
+
+
     // string constructor
+    CBigInt(const string &str) {
+        string result;
+        size_t i = 0;
+        if (str[i] == '-') {
+            m_is_negative = true;
+            i++;
+        } else {
+            m_is_negative = false;
+        }
+        for (i; i < str.size(); i++) {
+            if (str[i] == '0')
+                continue;
+            if (isdigit(str[i]))
+                result.push_back(str[i]);
+            throw invalid_argument("");
+        }
+
+        m_num = result;
+    }
+
+    CBigInt(string num, const bool is_negative) : m_num(std::move(num)), m_is_negative(is_negative) {}
+
     // operator +, any combination {CBigInt/int/string} + {CBigInt/int/string}
+    CBigInt operator+(const CBigInt &that) {
+        if (positive() && that.positive()) {
+            if (that.m_num.size() > m_num.size()) { // that is bigger
+                return {add(that.m_num, m_num), false};
+            } else { // this is bigger
+                return {add(m_num, that.m_num), false};
+            }
+        }
+        return {};
+    }
+
     // operator *, any combination {CBigInt/int/string} * {CBigInt/int/string}
     // operator +=, any of {CBigInt/int/string}
+    CBigInt &operator+=(const CBigInt &that) {
+        *this = *this + that;
+        return *this;
+    }
+
     // operator *=, any of {CBigInt/int/string}
     // comparison operators, any combination {CBigInt/int/string} {<,<=,>,>=,==,!=} {CBigInt/int/string}
     // output operator <<
+    friend ostream &operator<<(ostream &os, CBigInt num) {
+        string to_print = num.m_num;
+        reverse(to_print.begin(), to_print.end());
+        if (num.negative()) {
+            os << '-';
+        }
+        os << to_print;
+        return os;
+    }
     // input operator >>
 
-private:
+//private:
+    static string add(const string &bigger, const string &lesser) {
+        string result;
+        int carry = 0;
+        size_t i;
+        for (i = 0; i < bigger.size(); i++) {
+            int sum = carry;
+            sum += (bigger[i] - '0') + (lesser[i] - '0');
+            carry = sum / 10;
+            result.push_back((char) (sum % 10 + '0'));
+        }
+        while (carry) {
+            int sum = carry;
+            sum += (bigger[i] - '0');
+            carry = sum / 10;
+            result.push_back((char) (sum % 10 + '0'));
+        }
+        return result;
+    }
+
     string m_num;
     bool m_is_negative;
 };
 
 #ifndef __PROGTEST__
 
-//static bool equal(const CBigInt &x, const char val[]) {
-//    std::ostringstream oss;
-//    oss << x;
-//    return oss.str() == val;
-//}
-//
+static bool equal(const CBigInt &x, const char val[]) {
+    std::ostringstream oss;
+    oss << x;
+    return oss.str() == val;
+}
+
 //static bool equalHex(const CBigInt &x, const char val[]) {
 //    return true; // hex output is needed for bonus tests only
 //    // std::ostringstream oss;
@@ -70,8 +149,8 @@ int main() {
     CBigInt a, b;
     std::istringstream is;
     a = 10;
-//  a += 20;
-//  assert ( equal ( a, "30" ) );
+    a += 20;
+    assert (equal(a, "30"));
 //  a *= 5;
 //  assert ( equal ( a, "150" ) );
 //  b = a + 3;
