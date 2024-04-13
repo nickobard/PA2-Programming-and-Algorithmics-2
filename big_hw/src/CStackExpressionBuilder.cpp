399ec8a1c0d7159abbbc44198c5a0a1957a3b861
Nikita Bardatskii <bardanik@fit.cvut.cz>
2024-04-13 15:15:54 +0200

    big_task: dummy implementation of pure virtuals in stack builder

--- big_hw/src/CStackExpressionBuilder.cpp
+++ big_hw/src/CStackExpressionBuilder.cpp
@@ -3,3 +3,71 @@
 //
 
 #include "CStackExpressionBuilder.h"
+
+void CStackExpressionBuilder::opAdd() {
+
+}
+
+void CStackExpressionBuilder::opSub() {
+
+}
+
+void CStackExpressionBuilder::opMul() {
+
+}
+
+void CStackExpressionBuilder::opDiv() {
+
+}
+
+void CStackExpressionBuilder::opPow() {
+
+}
+
+void CStackExpressionBuilder::opNeg() {
+
+}
+
+void CStackExpressionBuilder::opEq() {
+
+}
+
+void CStackExpressionBuilder::opNe() {
+
+}
+
+void CStackExpressionBuilder::opLt() {
+
+}
+
+void CStackExpressionBuilder::opLe() {
+
+}
+
+void CStackExpressionBuilder::opGt() {
+
+}
+
+void CStackExpressionBuilder::opGe() {
+
+}
+
+void CStackExpressionBuilder::valNumber(double val) {
+
+}
+
+void CStackExpressionBuilder::valString(string val) {
+
+}
+
+void CStackExpressionBuilder::valReference(string val) {
+
+}
+
+void CStackExpressionBuilder::valRange(string val) {
+
+}
+
+void CStackExpressionBuilder::funcCall(std::string fnName, int paramCount) {
+
+}
--- big_hw/src/CStackExpressionBuilder.h
+++ big_hw/src/CStackExpressionBuilder.h
@@ -7,8 +7,46 @@
 
 #include "../expression.h"
 
+using namespace std;
 
 class CStackExpressionBuilder : public CExprBuilder {
+public:
+    void opAdd() override;
+
+    void opSub() override;
+
+    void opMul() override;
+
+    void opDiv() override;
+
+    void opPow() override;
+
+    void opNeg() override;
+
+    void opEq() override;
+
+    void opNe() override;
+
+    void opLt() override;
+
+    void opLe() override;
+
+    void opGt() override;
+
+    void opGe() override;
+
+    void valNumber(double val) override;
+
+    void valString(string val) override;
+
+    void valReference(string val) override;
+
+    void valRange(string val) override;
+
+    void funcCall(std::string fnName,
+                  int paramCount) override;
+
+private:
 
 };
 
