dc254ee220e3df47563cf84733207eab90e85fa4
Nikita Bardatskii <bardanik@fit.cvut.cz>
2024-04-14 16:16:55 +0200

    big_task: add include to parser

--- big_hw/src/ExpressionParser.h
+++ big_hw/src/ExpressionParser.h
@@ -1,7 +1,7 @@
 #ifndef expression_h_09845924528375
 #define expression_h_09845924528375
 
-
+#include "CExprBuilder.h"
 
 void parseExpression(std::string expr,
                      CExprBuilder &builder);
