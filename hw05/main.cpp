4a45469cc3f3c24747fb1988bc3e0d7bd24e5e83
Nikita Bardatskii <bardanik@fit.cvut.cz>
2024-04-27 19:30:01 +0200

    hw05: Add progtest template file

---
+++ hw05/main.cpp
@@ -0,0 +1,157 @@
+#ifndef __PROGTEST__
+#include <cassert>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <list>
+#include <algorithm>
+#include <memory>
+#include <functional>
+#endif /* __PROGTEST__ */
+
+class CComputer ...
+class CNetwork ...
+class CCPU ...
+class CMemory ...
+class CDisk ...
+
+#ifndef __PROGTEST__
+template<typename T_>
+std::string toString ( const T_ & x )
+{
+  std::ostringstream oss;
+  oss << x;
+  return oss . str ();
+}
+
+int main ()
+{
+  CNetwork n ( "FIT network" );
+  n . addComputer (
+        CComputer ( "progtest.fit.cvut.cz" ) .
+          addAddress ( "147.32.232.142" ) .
+          addComponent ( CCPU ( 8, 2400 ) ) .
+          addComponent ( CCPU ( 8, 1200 ) ) .
+          addComponent ( CDisk ( CDisk::MAGNETIC, 1500 ) .
+            addPartition ( 50, "/" ) .
+            addPartition ( 5, "/boot" ).
+            addPartition ( 1000, "/var" ) ) .
+          addComponent ( CDisk ( CDisk::SSD, 60 ) .
+            addPartition ( 60, "/data" )  ) .
+          addComponent ( CMemory ( 2000 ) ).
+          addComponent ( CMemory ( 2000 ) ) ) .
+      addComputer (
+        CComputer ( "courses.fit.cvut.cz" ) .
+          addAddress ( "147.32.232.213" ) .
+          addComponent ( CCPU ( 4, 1600 ) ) .
+          addComponent ( CMemory ( 4000 ) ).
+          addComponent ( CDisk ( CDisk::MAGNETIC, 2000 ) .
+            addPartition ( 100, "/" )   .
+            addPartition ( 1900, "/data" ) ) ) .
+      addComputer (
+        CComputer ( "imap.fit.cvut.cz" ) .
+          addAddress ( "147.32.232.238" ) .
+          addComponent ( CCPU ( 4, 2500 ) ) .
+          addAddress ( "2001:718:2:2901::238" ) .
+          addComponent ( CMemory ( 8000 ) ) );
+  assert ( toString ( n ) ==
+    "Network: FIT network\n"
+    "+-Host: progtest.fit.cvut.cz\n"
+    "| +-147.32.232.142\n"
+    "| +-CPU, 8 cores @ 2400MHz\n"
+    "| +-CPU, 8 cores @ 1200MHz\n"
+    "| +-HDD, 1500 GiB\n"
+    "| | +-[0]: 50 GiB, /\n"
+    "| | +-[1]: 5 GiB, /boot\n"
+    "| | \\-[2]: 1000 GiB, /var\n"
+    "| +-SSD, 60 GiB\n"
+    "| | \\-[0]: 60 GiB, /data\n"
+    "| +-Memory, 2000 MiB\n"
+    "| \\-Memory, 2000 MiB\n"
+    "+-Host: courses.fit.cvut.cz\n"
+    "| +-147.32.232.213\n"
+    "| +-CPU, 4 cores @ 1600MHz\n"
+    "| +-Memory, 4000 MiB\n"
+    "| \\-HDD, 2000 GiB\n"
+    "|   +-[0]: 100 GiB, /\n"
+    "|   \\-[1]: 1900 GiB, /data\n"
+    "\\-Host: imap.fit.cvut.cz\n"
+    "  +-147.32.232.238\n"
+    "  +-2001:718:2:2901::238\n"
+    "  +-CPU, 4 cores @ 2500MHz\n"
+    "  \\-Memory, 8000 MiB\n" );
+  CNetwork x = n;
+  auto c = x . findComputer ( "imap.fit.cvut.cz" );
+  assert ( toString ( *c ) ==
+    "Host: imap.fit.cvut.cz\n"
+    "+-147.32.232.238\n"
+    "+-2001:718:2:2901::238\n"
+    "+-CPU, 4 cores @ 2500MHz\n"
+    "\\-Memory, 8000 MiB\n" );
+  c -> addComponent ( CDisk ( CDisk::MAGNETIC, 1000 ) .
+         addPartition ( 100, "system" ) .
+         addPartition ( 200, "WWW" ) .
+         addPartition ( 700, "mail" ) );
+  assert ( toString ( x ) ==
+    "Network: FIT network\n"
+    "+-Host: progtest.fit.cvut.cz\n"
+    "| +-147.32.232.142\n"
+    "| +-CPU, 8 cores @ 2400MHz\n"
+    "| +-CPU, 8 cores @ 1200MHz\n"
+    "| +-HDD, 1500 GiB\n"
+    "| | +-[0]: 50 GiB, /\n"
+    "| | +-[1]: 5 GiB, /boot\n"
+    "| | \\-[2]: 1000 GiB, /var\n"
+    "| +-SSD, 60 GiB\n"
+    "| | \\-[0]: 60 GiB, /data\n"
+    "| +-Memory, 2000 MiB\n"
+    "| \\-Memory, 2000 MiB\n"
+    "+-Host: courses.fit.cvut.cz\n"
+    "| +-147.32.232.213\n"
+    "| +-CPU, 4 cores @ 1600MHz\n"
+    "| +-Memory, 4000 MiB\n"
+    "| \\-HDD, 2000 GiB\n"
+    "|   +-[0]: 100 GiB, /\n"
+    "|   \\-[1]: 1900 GiB, /data\n"
+    "\\-Host: imap.fit.cvut.cz\n"
+    "  +-147.32.232.238\n"
+    "  +-2001:718:2:2901::238\n"
+    "  +-CPU, 4 cores @ 2500MHz\n"
+    "  +-Memory, 8000 MiB\n"
+    "  \\-HDD, 1000 GiB\n"
+    "    +-[0]: 100 GiB, system\n"
+    "    +-[1]: 200 GiB, WWW\n"
+    "    \\-[2]: 700 GiB, mail\n" );
+  assert ( toString ( n ) ==
+    "Network: FIT network\n"
+    "+-Host: progtest.fit.cvut.cz\n"
+    "| +-147.32.232.142\n"
+    "| +-CPU, 8 cores @ 2400MHz\n"
+    "| +-CPU, 8 cores @ 1200MHz\n"
+    "| +-HDD, 1500 GiB\n"
+    "| | +-[0]: 50 GiB, /\n"
+    "| | +-[1]: 5 GiB, /boot\n"
+    "| | \\-[2]: 1000 GiB, /var\n"
+    "| +-SSD, 60 GiB\n"
+    "| | \\-[0]: 60 GiB, /data\n"
+    "| +-Memory, 2000 MiB\n"
+    "| \\-Memory, 2000 MiB\n"
+    "+-Host: courses.fit.cvut.cz\n"
+    "| +-147.32.232.213\n"
+    "| +-CPU, 4 cores @ 1600MHz\n"
+    "| +-Memory, 4000 MiB\n"
+    "| \\-HDD, 2000 GiB\n"
+    "|   +-[0]: 100 GiB, /\n"
+    "|   \\-[1]: 1900 GiB, /data\n"
+    "\\-Host: imap.fit.cvut.cz\n"
+    "  +-147.32.232.238\n"
+    "  +-2001:718:2:2901::238\n"
+    "  +-CPU, 4 cores @ 2500MHz\n"
+    "  \\-Memory, 8000 MiB\n" );
+  return EXIT_SUCCESS;
+}
+#endif /* __PROGTEST__ */
