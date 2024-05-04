db6689ecce8dc31c5f91ae268014bbbc0f458861
Nikita Bardatskii <bardanik@fit.cvut.cz>
2024-05-04 21:52:03 +0200

    big_task: Fix combine script

--- big_hw/assets/combine.sh
+++ big_hw/assets/combine.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-cd ..
-grep -vh '^#include' CPos.h CCycleDetectionVisitor.h CASTNode.h CASTExpressionBuilder.h CCell.h CLoader.h CSpreadsheet.h CPos.cpp CCycleDetectionVisitor.cpp CASTNode.cpp CASTExpressionBuilder.cpp CCell.cpp CLoader.cpp CSpreadsheet.cpp >| assets/all_in_one.cpp
+cd ../src
+grep -vh '^#include' \
+  SpreadsheetStructure/CPos.h \
+  ExpressionBuilders/CycleDetectionVisitor/CCycleDetectionVisitor.h \
+  ExpressionBuilders/ASTNodes/CASTNode.h \
+  ExpressionBuilders/ASTNodes/BinaryOperationNode.h \
+  ExpressionBuilders/ASTNodes/RelationalOperationNode.h \
+  ExpressionBuilders/ASTNodes/UnaryOperationNode.h \
+  ExpressionBuilders/CASTExpressionBuilder.h \
+  SpreadsheetStructure/CCell.h \
+  InputOutputUtilities/CLoader.h \
+  CSpreadsheet.h >| ../assets/all_in_one.cpp
+
+grep -vh '^#include' \
+  SpreadsheetStructure/CPos.cpp \
+  ExpressionBuilders/CycleDetectionVisitor/CCycleDetectionVisitor.cpp \
+  ExpressionBuilders/ASTNodes/CASTNode.cpp \
+  ExpressionBuilders/ASTNodes/BinaryOperationNode.cpp \
+  ExpressionBuilders/ASTNodes/RelationalOperationNode.cpp \
+  ExpressionBuilders/ASTNodes/UnaryOperationNode.cpp \
+  ExpressionBuilders/CASTExpressionBuilder.cpp \
+  SpreadsheetStructure/CCell.cpp \
+  InputOutputUtilities/CLoader.cpp \
+  CSpreadsheet.cpp >> ../assets/all_in_one.cpp
