dbffaa152331d561cf7d6f52ef3799796f3a195c
Nikita Bardatskii <bardanik@fit.cvut.cz>
2024-05-04 20:07:49 +0200

    big_task: move assets from source folder

--- big_hw/src/CRect.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by bardanik on 11/04/24.
-//
-#include "CRect.h"
-
--- big_hw/src/CRect.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by bardanik on 11/04/24.
-//
-
-#ifndef BARDANIK_CRECT_H
-#define BARDANIK_CRECT_H
-
-#endif //BARDANIK_CRECT_H
