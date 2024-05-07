9534333d0a48e3b981c8e19c58cc4f1309e7b474
Nikita Bardatskii <bardanik@fit.cvut.cz>
2024-05-07 20:19:38 +0200

    big_task: Document spreadsheet class.

--- big_hw/src/CSpreadsheet.cpp
+++ big_hw/src/CSpreadsheet.cpp
@@ -101,61 +101,6 @@ void CSpreadsheet::copyRect(CPos dst, CPos src, int w, int h) {
     range.paste(dst);
 }
 
-Cells CSpreadsheet::copyCellsAndShift(const CPos &src, const CPos &dst, int w, int h) {
-    Cells cells_shifted_copy;
-    auto [src_row, src_col] = src.getCoords();
-    auto offset = CPos::getOffset(src, dst);
-
-    auto row_beg = m_cells.lower_bound(src_row);
-    auto row_end = m_cells.upper_bound(src_row + h - 1);
-
-    while (row_beg != row_end) {
-        auto col_beg = row_beg->second.lower_bound(src_col);
-        auto col_end = row_beg->second.upper_bound(src_col + w - 1);
-        while (col_beg != col_end) {
-            auto cell_copy = shared_ptr<CCell>(col_beg->second->copy());
-            cell_copy->shift(offset);
-
-            CPos new_pos = {row_beg->first, col_beg->first};
-            new_pos.shift(offset);
-
-            setCell(cells_shifted_copy, new_pos, cell_copy);
-
-            col_beg++;
-        }
-        row_beg++;
-    }
-    return cells_shifted_copy;
-}
-
-void CSpreadsheet::deleteCells(const CPos &dst, int w, int h) {
-    auto [dst_row, dst_col] = dst.getCoords();
-    auto row_beg = m_cells.lower_bound(dst_row);
-    auto row_end = m_cells.upper_bound(dst_row + h - 1);
-    while (row_beg != row_end) {
-        auto col_beg = row_beg->second.lower_bound(dst_col);
-        auto col_end = row_beg->second.upper_bound(dst_col + w - 1);
-        row_beg->second.erase(col_beg, col_end);
-        if (row_beg->second.empty()) {
-            row_beg = m_cells.erase(row_beg);
-        } else {
-            row_beg++;
-        }
-    }
-}
-
-void CSpreadsheet::pasteCells(const Cells &cells) {
-    for (auto &row: cells) {
-        auto row_pos = row.first;
-        auto &columns = row.second;
-        for (auto &col: columns) {
-            auto col_pos = col.first;
-            auto cell = col.second;
-            setCell(m_cells, {row_pos, col_pos}, cell);
-        }
-    }
-}
-
 Cells &CSpreadsheet::getCells() {
     return m_cells;
 }
--- big_hw/src/CSpreadsheet.h
+++ big_hw/src/CSpreadsheet.h
@@ -15,48 +15,103 @@ constexpr unsigned SPREADSHEET_FILE_IO = 0x04;
 constexpr unsigned SPREADSHEET_SPEED = 0x08;
 constexpr unsigned SPREADSHEET_PARSER = 0x10;
 
-
+/**
+ * Class that represents a spreadsheet.
+ */
 class CSpreadsheet {
 public:
+    /**
+     * What functions the spreadsheet supports.
+     */
     static unsigned capabilities() {
         return SPREADSHEET_CYCLIC_DEPS | SPREADSHEET_FILE_IO | SPREADSHEET_FUNCTIONS;
     }
 
+    /**
+     * Default constructor.
+     */
     CSpreadsheet() = default;
 
+    /**
+     * Copy constructor - makes deep copy of cells.
+     * @param src - spreadsheet to make deep copy from.
+     */
     CSpreadsheet(const CSpreadsheet &src);
 
+    /**
+     * Copy-assignment operator - makes deep copy of cells.
+     * @param src - spreadsheet to make deep copy from.
+     * @return reference to the object where data was copied.
+     */
     CSpreadsheet &operator=(CSpreadsheet src);
 
+    /**
+     * Loads spreadsheet from any input stream.
+     * @param is - input stream to load this spreadsheet from.
+     * @return true if successfully loaded data to this spreadsheet, in case of error this data are not rewritten.
+     */
     bool load(istream &is);
 
+    /**
+     * Saves this spreadsheet to any output stream.
+     * @param os - output stream to save this spreadsheet data.
+     * @return true if successfully saved data, in case of error this data are not rewritten.
+     */
     bool save(ostream &os) const;
 
+    /**
+     * Set cell in spreadsheet with content. The content can be a number, string literal, or expression.
+     * @param pos - where to set content.
+     * @param contents - contents of the cell.
+     * @return true if cell is successfully set.
+     */
     bool setCell(CPos pos,
                  string contents);
 
+    /**
+     * Calculate value from some cell.
+     * @param pos - position of the cell to evaluate.
+     * @return value evaluated from the cell.
+     */
     CValue getValue(CPos pos);
 
+    /**
+     * Calculate value from position, but using provided cycle detection visitor.
+     * @param pos - position of the cell to evaluate.
+     * @param visitor - cycle detection visitor.
+     * @return value evaluated from the cell.
+     */
     CValue getValue(CPos pos, CCycleDetectionVisitor &visitor);
 
-
+    /**
+     * Copy rectangular portion of the spreadsheet and paste it to the another place,
+     * rewriting previous values.
+     * @param dst - upper left corner where to paste copied rectangular selection.
+     * @param src - upper left corner from which to copy rectangular selection.
+     * @param w - width of the rectangular selection, w >= 1.
+     * @param h - height of the rectangular selection, h>= 1.
+     */
     void copyRect(CPos dst,
                   CPos src,
                   int w = 1,
                   int h = 1);
 
+    /**
+     * Set cell in provided cells container.
+     * @param cells - container in which to set a cell.
+     * @param pos - position of the cell to evaluate.
+     * @param cell - cell to assign.
+     * @return true if the cell is successfully set.
+     */
     static bool setCell(Cells &cells, const CPos &pos, shared_ptr<CCell> &cell);
 
+    /**
+     * Get the container with cells of this spreadsheet.
+     * @return container with cells.
+     */
     Cells &getCells();
 
 private:
-
-
-    Cells copyCellsAndShift(const CPos &src, const CPos &dst, int w = 1, int h = 1);
-
-    void deleteCells(const CPos &dst, int w = 1, int h = 1);
-
-    void pasteCells(const Cells &cells);
 
     Cells m_cells;
 
